dbd2c5196edf9e7a8a3bcc36f88b45e9dfa05738
Ozodbek4252 <ozodbekmcfly@gmail.com>
2024-05-07 13:13:41 +0500

    Scetch of home page

---
+++ resources/views/livewire/home.blade.php
@@ -0,0 +1,21 @@
+<div class="w-full">
+    {{--  Header  --}}
+    <header>
+
+    </header>
+
+    {{--  Main  --}}
+    <main class="grid lg:grid-cols-12 gap-8 md:mt-10">
+
+        <aside class="lg:col-span-8 border overflow-hidden h-[1000px]">
+
+        </aside>
+        
+        {{--  Suggesstions  --}}
+        <aside class="lg:col-span-4 border lg:block p-4">
+
+        </aside>
+
+    </main>
+
+</div>
