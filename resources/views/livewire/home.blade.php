5c44ccecf94c6a9e0b4311ecc1a656d42592959c
Ozodbek4252 <ozodbekmcfly@gmail.com>
2024-05-07 13:23:01 +0500

    Home: stories

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -9,8 +9,20 @@
 
         <aside class="lg:col-span-8 border overflow-hidden h-[1000px]">
 
+            {{--  Stories  --}}
+            <section>
+                <ul class="flex overflow-x-auto items-center gap-2">
+                    @for ($i = 0; $i < 15; $i++)
+                        <li class="flex flex-col justify-center w-20 gap-1 p-2">
+                            <x-avatar story src="https://source.unsplash.com/500x500?face-{{ $i }}"
+                                class="h-14 w-14" />
+                            <p class="text-xs font-medium truncate">{{ fake()->name }}</p>
+                        </li>
+                    @endfor
+                </ul>
+            </section>
         </aside>
-        
+
         {{--  Suggesstions  --}}
         <aside class="lg:col-span-4 border lg:block p-4">
 
