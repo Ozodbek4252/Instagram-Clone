c0e71aef840ea67b2dbbce19b75de6ade8832b39
Ozodbek4252 <ozodbekmcfly@gmail.com>
2024-05-31 10:15:19 +0500

    Front: Post: direct reply to post in home

--- app/Livewire/Post/Item.php
+++ app/Livewire/Post/Item.php
@@ -7,9 +7,24 @@ use Livewire\Component;
 class Item extends Component
 {
     public $post;
+    public $body;
 
     public function render()
     {
         return view('livewire.post.item');
+    }
+
+    public function addComment()
+    {
+        $this->validate([
+            'body' => 'required'
+        ]);
+
+        $this->post->comments()->create([
+            'body' => $this->body,
+            'user_id' => auth()->id()
+        ]);
+
+        $this->body = '';
     }
 }
